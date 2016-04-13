f417f51033f223eea5cdc9117c17bad46d3bff01
PCS0025 <amishkin@space-team.com>
2016-04-13 14:55:23 +0300

    интерфей на перспективу

--- ST/pars/PosParser.py
+++ ST/pars/PosParser.py
@@ -11,6 +11,7 @@ def max_fuel_value(file, at):
     if len(x) > 0:
         return max(x)
 
+
 def log_dir():
     path = open('C:\STMatix\STM Diagnostics\STM_Diagnostics.ini', 'r')
     diagn_ini = path.read()
@@ -21,25 +22,35 @@ def log_dir():
         log_path = x[0]
         return log_path
 
-def main():
+
+def max_fuel(at):
     if log_dir() != None:
-        while True:
-            at = input('АТ (\'q\' для выхода ): ')
-            if at == 'q':
-                break
+        for x in range(1, 32):
+            if x < 10:
+                pos_path = log_dir() + '\\' + '0' + str(x) + '.log'
             else:
-                for x in range(1, 32):
-                    if x < 10:
-                        pos_path = log_dir() + '\\' + '0' + str(x) + '.log'
-                    else:
-                        pos_path = log_dir() + '\\' + str(x) + '.log'
-                    print('День ', x)
-                    if os.path.exists(pos_path):
-                        print('Максимальное показание датчика: ', max_fuel_value(pos_path, int(at)))
-                    else:
-                        print('Лог не найден')
+                pos_path = log_dir() + '\\' + str(x) + '.log'
+            print('День ', x)
+            if os.path.exists(pos_path):
+                print('Максимальное показание датчика: ', max_fuel_value(pos_path, int(at)))
+            else:
+                print('Лог не найден')
     else:
         print('Папка с логами не найдена')
+
+
+def main():
+    while True:
+        print('Выберите режим : ')
+        print('1 - Максимальное показание датчика топлива за месяц')
+        print('0 - Выход')
+        choise = int(input())
+        if choise == 1:
+            device_code = int(input('АТ: '))
+            max_fuel(device_code)
+        if choise == 0:
+            break
+
 
 if __name__ == '__main__':
     main()
